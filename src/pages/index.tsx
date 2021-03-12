53f0bb7fc2175a61362f481bc900791728736a2d
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-03-12 00:56:25 +0000

    Update: Ignored next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 
 import Head from 'next/head';
 
+import ChallengeBox from '../components/ChallengeBox';
 import CompletedChallenges from "../components/CompletedChallenges";
 import Countdown from "../components/Countdown";
 import ExperienceBar from "../components/ExperienceBar";
@@ -25,7 +26,7 @@ export default function Home() {
       <Countdown />
     </div>
     <div>
-
+      <ChallengeBox />
     </div>
     </section>
    </div>
